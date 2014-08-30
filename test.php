472325bb4ff27957db2e6a5cc204fd6d713041bc
Martin Ma <martin.ma@bluecomgroup.com>
2014-08-30 22:36:04 +0800

    webhooks test

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 error_reporting ( E_ALL );
 $dir = '/var/www/magento/';//该目录为git检出目录
 $handle = popen('cd '.$dir.' && git pull 2>&1','r');
+var_dump($handle);
 $read = stream_get_contents($handle);
 printf($read);
 pclose($handle);
